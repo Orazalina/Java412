32b0fbcd04a340c2306939bffd62ccbed9f1b1cd
Anara Orazalina <orazalina.anara@inbox.ru>
2021-03-23 23:54:01 +0600

    added

---
+++ src/main/java/ru/netology/domain/Book.java
@@ -0,0 +1,18 @@
+package ru.netology.domain;
+
+public class Book extends Product {
+    private String autor;
+    private int pages;
+    private int publishedYear;
+
+    public Book(int id, String name, int price, String autor, int pages, int publishedYear) {
+        super(id, name, price);
+        this.autor = autor;
+        this.pages = pages;
+        this.publishedYear = publishedYear;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+}
